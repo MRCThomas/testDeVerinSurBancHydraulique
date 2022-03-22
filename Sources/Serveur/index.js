8112cbea298e7c0f192981a88595080a7dd77900
Hugo <fosse.hugo25@gmail.com>
2022-03-22 17:23:57 +0100

    Exemple Recherche Affaire
    Mise en place d'un exemple d'utilisation de recherche d'affaire

--- Sources/Serveur/index.js
+++ Sources/Serveur/index.js
@@ -13,14 +13,27 @@ app.get('/', function (req, res) {
 app.get('/welcome', function (req, res) {
     res.send('<b>Hello</b> welcome to my http server made with express');
 });
-app.post('/welcome', function (req, res){
-    console.log(req);
+app.post('/welcome', function (req, res) {
+    console.log(req.body);
     res.json({
-        "hello": "world"
+        "hello": req.body.name
     })
 })
+app.get('/api/affaire/:id', function (req, res, next) {
+    const affaires = [
+        { id: 24, name: "affaire3" },
+        { id: 25, name: "affaire25" },
+        { id: 45, name: "affaire45" },
+        { id: 67, name: "affaire67" },
+        { id: 34, name: "affaire34" },
+        { id: 94, name: "affaire94" },
+
+    ];
+    const id = req.params.id;
+    res.json(affaires.filter((e) => e.id == id));
+})
 // Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
 
