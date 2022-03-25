be7853ee1a2b8a4ba44a32396f828738dddff13c
MRCThomas <73233904+MRCThomas@users.noreply.github.com>
2022-03-25 10:50:54 +0100

    corection index et style

--- Sources/Application Web/index.php
+++ Sources/Application Web/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link type="text/javascript" href="../Serveur/index.js">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Document</title>
 </head>
@@ -14,7 +15,7 @@
     <form>
         <h1>Poste Contrôleur</h1>
         <div class="logo">
-            <p><img src="/logoPc.svg" alt="logoPc"></p>
+            <p><img src="./img/logoPc.svg" alt="logoPc"></p>
         </div>
         <p></p>
         <div class="inputs">
