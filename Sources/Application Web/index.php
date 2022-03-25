99bc1e0379b6400be4262a9371c39695906f545c
MRCThomas <73233904+MRCThomas@users.noreply.github.com>
2022-03-25 10:01:34 +0100

    🎨 commit index.php and style.css

---
+++ Sources/Application Web/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <h1>Poste Contrôleur</h1>
+        <div class="logo">
+            <p><img src="/logoPc.svg" alt="logoPc"></p>
+        </div>
+        <p></p>
+        <div class="inputs">
+            Identifiant
+            <input type="email" placeholder="identifiant">
+            mot de passe
+            <input type="password" placeholder="mot de passe">
+        </div>
+
+        <p></p>
+
+        <div align=center>
+            <button type="submit">Connexion</button>
+        </div>
+    </form>
+</body>
+
+</html>
