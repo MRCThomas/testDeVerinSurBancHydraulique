b782b8c92d0a82718688bad463337b8b71983cec
Hugo <fosse.hugo25@gmail.com>
2022-03-25 15:27:53 +0100

    Ajout d'une route
    Ajout d'une route /api/affaire/fake

--- Sources/Serveur/index.js
+++ Sources/Serveur/index.js
@@ -1,8 +1,9 @@
 // Création d'une instance d'express
 const { response } = require('express');
 var express = require('express');
+var cors = require('cors');
 var app = express();
-
+app.use(cors());
 app.use(express.static('./../Application Web'), express.json()); //
 
 app.post('/welcome', function (req, res) {
@@ -11,6 +12,14 @@ app.post('/welcome', function (req, res) {
         "hello": req.body.name
     })
 })
+app.get('/api/affaire/fake' , function (req, res, next) {
+    const data = []
+    for(let i = 0; i < 50;i++){
+        data.push(Math.random() * 100);
+    }
+    res.json({data});
+})
+
 app.get('/api/affaire/:id', function (req, res, next) {
     const affaires = [
         { id: 24, name: "affaire3" },
