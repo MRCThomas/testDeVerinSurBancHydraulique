73546158d220a6fc09773c9413d78c97b595cdd4
EnseignantLapro <btssnpro@gmail.com>
2022-05-06 15:36:31 +0200

    Commit SQL

--- Sources/Serveur/index.js
+++ Sources/Serveur/index.js
@@ -1,8 +1,7 @@
 // Création d'une instance d'express
 import express from 'express';
 import cors from 'cors';
-import db from "./services/db.js";
-import WebSocket from 'ws';
+import { query } from "./services/db.js";
 import jwt from 'jsonwebtoken';
 import authMiddleware from './authentification-middleware.js';
 import { Server }  from 'http';
@@ -14,6 +13,7 @@ const wsServeur = new io.Server(serveur);
 app.use(cors());
 app.use(express.static('./../Application Web'), express.json()); //
 app.use(authMiddleware)
+
 app.get('/affaire/fake' ,function (req, res, next) {   // Route de simulation de données concernants les affaires 
     const data = []
     for(let i = 0; i < 50;i++){
@@ -22,19 +22,24 @@ app.get('/affaire/fake' ,function (req, res, next) {   // Route de simulation de
     res.json({data});
 })
 
+app.get('/api/testDB', async (req,res,next) => {
+    const result = await query(`SELECT * FROM clients;`);
+    res.json({ 
+        'hello': 'world',
+        'result': result
+    });
+})
 
-app.post('/api/login/', function (req, res, next) {     //Route pour vérifier la connexion du contrôleur
-    const users = [                                     //Simulation de la table 'Users' de la base de données
-        {username: "root", pwd: "root"},
-        {username: "admin", pwd: "1234"},
-    ];
-    const found = users.find(u=>u.username === req.body.username);
-    if(!found){
+app.post('/api/login/',  async (req, res, next) =>  {     //Route pour vérifier la connexion du contrôleur
+    console.log(`SELECT * FROM users WHERE Identifiants = '${req.body.username}';`);
+    const user = await query(`SELECT * FROM users WHERE Identifiants = ${req.body.username};`)
+    console.log(user);
+    if(!user){
         return res.status(403).send();                  //Retourne 403 pour une connexion échouée
     }
-    if(found.pwd === req.body.pwd){
-        let token = jwt.sign({ user: found}, 'secret'); //Délivre un token d'authentification
-        return res.status(200).json(token);             //Retourne 200 pour une connexion réussie
+    if(user.pwd === req.body.pwd){
+        let token = jwt.sign({ user: user}, 'secret'); //Délivre un token d'authentification
+        return res.status(200).json({"access_token" : token});             //Retourne 200 pour une connexion réussie
     }else{
         return res.status(403).send();
     }
