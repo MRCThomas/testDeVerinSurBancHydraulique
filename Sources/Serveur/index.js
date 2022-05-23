16f0324e43f54be940bb35bc118f628a099f497c
EnseignantLapro <btssnpro@gmail.com>
2022-05-23 17:47:17 +0200

    modif server wss

--- Sources/Serveur/index.js
+++ Sources/Serveur/index.js
@@ -1,18 +1,18 @@
 // Création d'une instance d'express
 import express from 'express';
+import * as http from 'http';
+import { WebSocketServer }  from 'ws';
+
 import cors from 'cors';
 import { query } from "./services/db.js";
 import jwt from 'jsonwebtoken';
 import authMiddleware from './authentification-middleware.js';
-import { Server }  from 'http';
-import * as io from 'socket.io';
+
 const app = express();
-const serveur = Server(app);
-const wsServeur = new io.Server(serveur);
 
 app.use(cors());
 app.use(express.static('./../Application Web'), express.json()); //
-app.use(authMiddleware)
+//app.use(authMiddleware)
 
 app.get('/affaire/fake' ,function (req, res, next) {   // Route de simulation de données concernants les affaires 
     const data = []
@@ -31,8 +31,7 @@ app.get('/api/testDB', async (req,res,next) => {
 })
 
 app.post('/api/login/',  async (req, res, next) =>  {     //Route pour vérifier la connexion du contrôleur
-    console.log(`SELECT * FROM users WHERE Identifiants = '${req.body.username}';`);
-    const user = await query(`SELECT * FROM users WHERE Identifiants = ${req.body.username};`)
+    const user = await query(`SELECT * FROM users WHERE Identifiants = '${req.body.username}';`)
     console.log(user);
     if(!user){
         return res.status(403).send();                  //Retourne 403 pour une connexion échouée
@@ -51,12 +50,28 @@ app.use(function (req, res, next) {
 });
 
 
-// Lance le serveur sur le port 3000 (WS)
-wsServeur.on('connection', (socket) =>{
-    console.log(`Connecté au client ${socket.id}`)
- })
+try {
+    
+
+const server = http.createServer(app);
+console.log(`Démarrage du serveur HTTP`);
+const wss = new WebSocketServer({ server });
+console.log(`Démarrage du serveur WebSocket`);
+console.log(wss);
+wss.on('connection',(ws) => {
+    ws.on('message',(message) => {
+        console.log(`received : ${ message}`);
+        ws.send(`Hello, you sent -> ${message}`);
+    })
+
+    ws.send(`Hi there, I am a WebSocket Server`);
+})
 
 // Lance le serveur sur le port 3000 (HTTP)
-serveur.listen(3000, function () {
+server.listen(3000, () => {
     console.log('API TestVerin démarrée et disponible à l\'adresse : http://localhost:3000.');
 });
+
+} catch (error) {
+    console.error(error)    
+}
