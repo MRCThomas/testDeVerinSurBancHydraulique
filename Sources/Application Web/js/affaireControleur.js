9df17ac325ff311bfe5447a9af166f8ddefe6826
Hugo <fosse.hugo25@gmail.com>
2022-05-31 14:23:56 +0200

    affaireControleur.js
    Fonction pour ajouter des éléments au DOM

--- Sources/Application Web/js/affaireControleur.js
+++ Sources/Application Web/js/affaireControleur.js
@@ -1,71 +1,149 @@
 let addAffaire = document.getElementById("addAffaireButton");
 let listeAffaires = document.getElementById("affaires");
 let logOffButton = document.getElementById("logOffIcon");
+let tableAffaires;                                          // Variable contenat la table Affaire de la BDD
+let tableEssais;                                            // Variable contenant la table Essais de la BDD
+let ipAdress = "localhost";                            // Ip du poste sur lequel le code est executer
 
-function createListeElement(affaires) {
-    console.log(affaires)
-    let affaireListItem = document.createElement('li');
-    affaireListItem.classList.add("menu-deroulant");
-    let anchor = document.createElement('a');
-    anchor.textContent = `Affaires n° ${affaires.IdAffaire}`
-    affaireListItem.appendChild(anchor);
-    let unorderedList = document.createElement('ul');
-    unorderedList.classList.add('dropdown-content');
-    // normalement pour chaque essai de l'affaire .
-    essaiListItem = document.createElement('li');
+function createEssaiElement(essai) {
+
+    let dropdownContent = document.createElement('ul');
+    dropdownContent.classList.add('dropdown-content');
+
+    let essaiListItem = document.createElement('li');
     let anchorEssai = document.createElement('a');
-    anchorEssai.textContent = `Essai n° ${affaires.IdEssai}`;
+    anchorEssai.textContent = `Essai n° ${essai.IdEssai}`;
     essaiListItem.appendChild(anchorEssai);
+
     images = ['./img/blueChart.svg','./img/bluePen.svg','./img/redTrash.svg'];
     images.forEach(src => {
+
         let img = document.createElement('img');
         img.src=src;
         img.classList.add('modifIcons');
         essaiListItem.appendChild(img);
-    })
-    unorderedList.appendChild(essaiListItem);
-    affaireListItem.appendChild(unorderedList);
 
-    return affaireListItem;
-    
+    })
+    dropdownContent.appendChild(essaiListItem);
+    return dropdownContent
 }
 
-fetch("http://localhost:3000/api/afficheAffaires", {
-    "headers": { "authorization": `Bearer ${localStorage.getItem("Authorization")}` }
-})
-    .then(response => {
-        response.json().then((json) => {
-            console.log(json);
-            json.forEach(affaire => {
-                listeAffaires.appendChild(createListeElement(affaire))
-            });
+function createAffaireElement(affaire) {
+    let affaireContent = document.createElement('li');
+    affaireContent.classList.add("menu-deroulant");
 
-        })
-    })
+    let anchor = document.createElement('a');
+    anchor.textContent = `Affaires n° ${affaire.IdAffaire}`;
+    affaireContent.appendChild(anchor);
 
-fetch("http://localhost:3000/API/afficheEssais", {
-    "headers": { "authorization": `Bearer ${localStorage.getItem("Authorization")}` }
-})
-    .then(response => {
-        response.json().then((json) => {
-            console.log(json);
-        })
-    })
+    return affaireContent
+}
+
+function createListeElement(affaire, tableEssais) {         // Fonction qui instancie une affaire contenant ses essais sur l'interface
+
+    let affaireListItem = document.createElement('li');
+    affaireListItem.classList.add("menu-deroulant");
+
+    let anchor = document.createElement('a');
+    anchor.textContent = `Affaires n° ${affaire.IdAffaire}`;
+    affaireListItem.appendChild(anchor);
+
     
+
+    tableEssais.forEach(essai => {      //On boucle sur tout les essais
+
+        if(essai.IdAffaire == affaire.IdAffaire){       //Si l'id de l'essai correspond a l'id de l'essai contenu dans l'affaire
+
+            let essaiListItem = document.createElement('li');
+            let anchorEssai = document.createElement('a');
+            anchorEssai.textContent = `Essai n° ${essai.IdEssai}`;
+            essaiListItem.appendChild(anchorEssai);
+
+            images = ['./img/blueChart.svg','./img/bluePen.svg','./img/redTrash.svg'];
+            images.forEach(src => {
+
+                let img = document.createElement('img');
+                img.src=src;
+                img.classList.add('modifIcons');
+                essaiListItem.appendChild(img);
+
+            })
+
+            unorderedList.appendChild(essaiListItem);
+            affaireListItem.appendChild(unorderedList);
+
+            return affaireListItem;
+        }
+    });
+}
+
+fetch("http://" + ipAdress + ":3000/API/getTableEssais", {
+
+    "headers": { "authorization": `Bearer ${localStorage.getItem("Authorization")}` }
+
+})
+    .then(response => {
+
+        response.json().then((json) => {
+
+            tableEssais = json;
+        })
+    })
+
+fetch("http://" + ipAdress + ":3000/api/getTableAffaires", {
+
+    "headers": { "authorization": `Bearer ${localStorage.getItem("Authorization")}` }
+
+})
+    .then(response => {
+        response.json().then((json) => {
+
+            tableAffaires = json;
+            let essaisOk = null;
+            console.log(tableAffaires);
+            
+            tableAffaires.forEach(affaire => {
+                console.log(tableEssais);
+                tableEssais.forEach(essai => {
+                    
+                    if(affaire.IdAffaire == essai.IdAffaire){
+                        
+                        essaisOk.push(createEssaiElement(essai));
+                        
+                    }
+                })
+
+                let displayedAffaire = createAffaireElement(affaire)
+
+                essaisOk.forEach(essai => {
+
+                    displayedAffaire.appendChild(essai);
+                })
+                listeAffaires.appendChild(displayedAffaire);
+            });
+        })
+    })
+
 addAffaire.addEventListener("click", (e) => {
 
     if (confirm("Etes vous sure de vouloir créer une affaire ?")) {
-        const res = fetch("http://localhost:3000/api/newAffaire", {
-            "headers": { "authorization": `Bearer ${localStorage.getItem("Authorization")}` }
+
+        fetch("http://" + ipAdress + ":3000/api/newAffaire", {
+            method : 'POST',
+            headers: { "authorization": `Bearer ${localStorage.getItem("Authorization")}` },
+            body : JSON.stringify({
+                "user" : localStorage.getItem("username")
+            })
         });
-        console.log(res.json);
+        
     }
 })
 
 logOffButton.addEventListener("click", (e) => {
 
     if(confirm("Etes vous sure de vouloir vous déconnecter ?")) {
-        req.headers.authorization = null;
-        window.location.href = 'http://localhost:3000';
+
+        window.location.href = 'http://' + ipAdress + ':3000';
     }
 })
+
