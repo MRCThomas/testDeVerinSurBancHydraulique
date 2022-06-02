daf1a90f869c627d8fa3c20944d9a491824de885
EnseignantLapro <btssnpro@gmail.com>
2022-06-02 16:17:14 +0200

    Robin : restauration de la bdd depuis un dump

--- Sources/Serveur/index.js
+++ Sources/Serveur/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import * as http from 'http';
-import { WebSocketServer }  from 'ws';
+import { WebSocketServer } from 'ws';
 
 import cors from 'cors';
 import { query } from "./services/db.js";
 import jwt from 'jsonwebtoken';
+import { spawn } from 'child_process';
 import authMiddleware from './authentification-middleware.js';
+import Importer from 'mysql-import';
+import { join,dirname } from 'path';
+
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -13,38 +20,26 @@ app.use(cors());
 
 
 app.get('/restaurationVierge', (req, res, next) => {
-    // on définie nos variables
-    const DB_USER = "root"
-    const DB_PWD = "root"
-    const DB_HOST = "127.0.0.1"
-    const dirPath = `./sauvegardes/BddVierge`
-    const { exec } = require("child_process");
-    const verif = `mysqlquery( 'SHOW DATABASE LIKE "testverins" 'or die(mysql_error()))`;
-    const drop = query( `DROP DATABASE testverins` )
-    drop
-    const command = `mysql -u ${DB_USER} -p${DB_PWD}  -h ${DB_HOST} ` 
-    console.log(command)   
-    exec(`${command} ${verif}`),(error)=> {
-    if( error ){
-        console.log('ici!')
-         // on restaure la sauvegardes testverins.sql dans le dossier BddVierge
-        exec(`${command} testverins < ${dirPath}/testverins.sql`, (error, stdout, stderr) => { 
-            if (error) {
-                next(error)
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-            }
-       
-        })
-    }
-    else{
-        exec(`${command} DROP DATABASE testverins`), (error) => {
-            console.log('la!')
-        }
-    }
-}
+    const host = 'localhost';
+    const user = 'root';
+    const password = 'root';
+    //const database = 'testverins';
+
+    const importer = new Importer({ host, user, password });
+
+    // New onProgress method, added in version 5.0!
+    importer.onProgress(progress => {
+        var percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100;
+        console.log(`${percent}% Completed`);
+    });
+
+    importer.import(join(__dirname,'./sauvegardes/BddVierge/testverins.sql')).then(() => {
+        var files_imported = importer.getImported();
+        console.log(`${files_imported.length} SQL file(s) imported.`);
+        res.send('completed');
+    }).catch(err => {
+        console.error(err);
+    });
 })
 
 
@@ -57,10 +52,10 @@ app.get('/sauvegarde', (req, res, next) => {
     const dirPath = `./sauvegardes/${new Date().toLocaleDateString().replaceAll('/', '-')}`
     fs.mkdirSync(dirPath)  // On créér un dossier à l'emplacement voulu
     const { exec } = require("child_process");
-    const command = `mysqldump -u ${DB_USER} -p${DB_PWD} testverins -h ${DB_HOST}` 
+    const command = `mysqldump -u ${DB_USER} -p${DB_PWD} testverins -h ${DB_HOST}`
     console.log(command)
     // on créer la sauvegardes testverins.sql dans le dossier créé Précédement
-    exec(`${command} > ${dirPath}/testverins.sql`, (error, stdout, stderr) => { 
+    exec(`${command} > ${dirPath}/testverins.sql`, (error, stdout, stderr) => {
         if (error) {
             next(error)
             return;
@@ -69,7 +64,7 @@ app.get('/sauvegarde', (req, res, next) => {
             console.error(`stderr: ${stderr}`);
         }
 
-        const sentFilePath = path.join(__dirname,dirPath);
+        const sentFilePath = path.join(__dirname, dirPath);
         res.sendFile(`${sentFilePath}/testverins.sql`, function (err) {
             console.log('ici !')
             if (err) {
@@ -84,16 +79,16 @@ app.get('/sauvegarde', (req, res, next) => {
 app.use(express.static('./../Application Web'), express.json()); //
 //app.use(authMiddleware)
 
-app.get('/affaire/fake' ,function (req, res, next) {   // Route de simulation de données concernants les affaires 
+app.get('/affaire/fake', function (req, res, next) {   // Route de simulation de données concernants les affaires 
     const data = []
     const essai = db.query("SELECT * from essai")  // On reçois tout les donnés d'une Affaires 
-        result:
-             result()
-    res.json({essai});
+    result:
+    result()
+    res.json({ essai });
 })
 
 app.post('/api/newAffaire', async (req, res, next) => {
-    
+
     try {
         const IdUser = await query(`SELECT IdUser FROM users WHERE Identifiants = '${req.body.user}';`)
         console.log(req.body.user);
@@ -119,7 +114,7 @@ app.get('/api/getTableAffaires', async (req, res, next) => {    //Route pour rec
 
 app.get('/api/getTableEssais', async (req, res, next) => {
 
-    try{
+    try {
         const tableEssais = await query('SELECT * FROM essais');
         return res.json(tableEssais);
     } catch (error) {
@@ -127,16 +122,16 @@ app.get('/api/getTableEssais', async (req, res, next) => {
     }
 })
 
-app.post('/api/login/',  async (req, res, next) =>  {     //Route pour vérifier la connexion du contrôleur
+app.post('/api/login/', async (req, res, next) => {     //Route pour vérifier la connexion du contrôleur
     const user = await query(`SELECT * FROM users WHERE Identifiants = '${req.body.username}';`);
-    if(!user.length >= 1){
+    if (!user.length >= 1) {
         return res.status(403).send();              //Retourne 403 pour une connexion échouée
 
     }
-    if(user[0].MDP === req.body.pwd){
-        let token = jwt.sign({ user: user}, 'secret'); //Délivre un token d'authentification
-        return res.status(200).json({"access_token" : token});             //Retourne 200 pour une connexion réussie
-    }else{
+    if (user[0].MDP === req.body.pwd) {
+        let token = jwt.sign({ user: user }, 'secret'); //Délivre un token d'authentification
+        return res.status(200).json({ "access_token": token });             //Retourne 200 pour une connexion réussie
+    } else {
         return res.status(403).send();
     }
 });
@@ -147,23 +142,23 @@ app.use(function (req, res, next) {
 });
 
 try {
-  
-  const server = http.createServer(app);
-  console.log(`Démarrage du serveur HTTP`);
-  const wss = new WebSocketServer({ server });
-  console.log(`Démarrage du serveur WebSocket`);
-  wss.on('connection',(ws) => {
-      ws.on('message',(message) => {
-          console.log(`received : ${ message}`);
-          ws.send(`Hello, you sent -> ${message}`);
-      })
 
-      ws.send(`Hi there, I am a WebSocket Server`);
-  })
-  
-  server.listen(3000, () => {
-    console.log('API TestVerin démarrée et disponible à l\'adresse : http://localhost:3000.');
-  });
+    const server = http.createServer(app);
+    console.log(`Démarrage du serveur HTTP`);
+    const wss = new WebSocketServer({ server });
+    console.log(`Démarrage du serveur WebSocket`);
+    wss.on('connection', (ws) => {
+        ws.on('message', (message) => {
+            console.log(`received : ${message}`);
+            ws.send(`Hello, you sent -> ${message}`);
+        })
+
+        ws.send(`Hi there, I am a WebSocket Server`);
+    })
+
+    server.listen(3000, () => {
+        console.log('API TestVerin démarrée et disponible à l\'adresse : http://localhost:3000.');
+    });
 } catch (error) {
-    console.error(error)    
+    console.error(error)
 }
